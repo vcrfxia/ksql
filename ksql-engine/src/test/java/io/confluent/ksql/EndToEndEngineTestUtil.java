7d14c1db798a650c4ebc1f5c75828060f1723b10
Bill Bejeck <bill@confluent.io>
2018-10-03 12:02:14 -0400

    Close KsqlEngine, can get rid of random port and de-registers metrics

--- ksql-engine/src/test/java/io/confluent/ksql/EndToEndEngineTestUtil.java
+++ ksql-engine/src/test/java/io/confluent/ksql/EndToEndEngineTestUtil.java
@@ -558,21 +558,19 @@ final class EndToEndEngineTestUtil {
 
   static void writeExpectedTopologyFiles(final String topologyDir, List<Query> queryList) {
 
-    final Random randomPort = new Random();
     final ObjectWriter objectWriter = new ObjectMapper().writerWithDefaultPrettyPrinter();
     final MockSchemaRegistryClient mockSchemaRegistryClient = new MockSchemaRegistryClient();
     final Supplier<SchemaRegistryClient> schemaRegistryClientFactory = () -> mockSchemaRegistryClient;
     queryList.forEach(query -> {
       final Map<String, Object> originalConfigs = getConfigs(null);
       final Map<String, Object> updatedConfigs = new HashMap<>(originalConfigs);
-      // need to overwrite the bootstrap servers for generating file
-      updatedConfigs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:" + randomPort.nextInt(4000));
 
       final KsqlConfig ksqlConfig = new KsqlConfig(ImmutableMap.copyOf(updatedConfigs));
       final KsqlEngine ksqlEngine = getKsqlEngine(schemaRegistryClientFactory, ksqlConfig);
       final Topology topology = getStreamsTopology(query, ksqlEngine, ksqlConfig);
       final Map<String, String> configsToPersist = ksqlConfig.getAllConfigPropsWithSecretsObfuscated();
       writeExpectedTopologyFile(query.name, topology, configsToPersist, objectWriter, topologyDir);
+      ksqlEngine.close();
     });
   }
 
